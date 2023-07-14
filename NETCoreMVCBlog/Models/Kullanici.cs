3c5b14d95f256c70e687f03aee178703ec0906a0
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-07-14 14:51:18 +0300

    Models içerisinde "Kullanici" Class'ına, "Kullanıcı Adı" "Elektronik Posta" Display Annotation eklemeleri yapıldı.

--- NETCoreMVCBlog/Models/Kullanici.cs
+++ NETCoreMVCBlog/Models/Kullanici.cs
@@ -1,9 +1,16 @@
-﻿namespace NETCoreMVCBlog.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NETCoreMVCBlog.Models
 {
     public class Kullanici
     {
         public int Id { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
         public string KullaniciAdi { get; set; }
+        [Display(Name = "Elektronik Posta")]
         public string Email { get; set; }
     }
 }
